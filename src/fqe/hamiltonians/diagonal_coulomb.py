59f939528108c19ceda45db9c7b69d5e09988614
Ryan LaRose <rlarose@umich.edu>
2020-11-16 19:11:53 -0500

    Linting.

--- src/fqe/hamiltonians/diagonal_coulomb.py
+++ src/fqe/hamiltonians/diagonal_coulomb.py
@@ -11,7 +11,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-"""Defines class for the DiagonalCoulomb Hamiltonian."""
+"""Defines the DiagonalCoulomb Hamiltonian class."""
 from typing import Dict
 
 import numpy as np
