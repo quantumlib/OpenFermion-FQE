2a30971971d8b5ae01762d1e57fda4d1ff4a21d5
Ryan LaRose <rlarose@umich.edu>
2020-11-25 23:00:12 -0500

    Format docstrings and lint.

--- src/fqe/cirq_utils.py
+++ src/fqe/cirq_utils.py
@@ -11,17 +11,16 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+"""Utilities for interfacing with Cirq."""
 
-"""Utilities which specifically require import from Cirq
-"""
-#Type checking splits the imports
-#pylint: disable=ungrouped-imports
+# Type checking splits the imports
+# pylint: disable=ungrouped-imports
 
 from typing import List, TYPE_CHECKING
 
-import numpy
+import numpy as np
 
-from cirq import X, Y, Z, Moment, Circuit, Simulator, LineQubit
+from cirq import X, Y, Z, Moment, Circuit, Simulator, Qid
 from openfermion import QubitOperator
 
 from fqe.util import init_qubit_vacuum
@@ -30,100 +29,94 @@ if TYPE_CHECKING:
     from cirq.ops.pauli_string import SingleQubitPauliStringGateOperation
 
 
-def qubit_ops_to_circuit(ops: 'QubitOperator', qpu: List[LineQubit]) -> 'Circuit':
-    """Generate a circuit that can be run on a Cirq simulator from the ops
-    passed
+def qubit_ops_to_circuit(
+    ops: "QubitOperator", qubits: List[Qid]
+) -> "Circuit":
+    """Returns a Cirq circuit from the input ops and qubits.
 
     Args:
-        ops (Qubit Operator) - a product of operations to compile into a
-            circuit
-        qpu (Qid) - the quantum processing unit that this circuit will be run
-            on
+        ops: A product of operations to compile into a circuit.
+        qubits: List of qubits the operators should act on.
 
     Returns:
-        (circuit) - returns a circuit to run in cirq
+        A Cirq circuit representation of the input ops.
     """
     gates = []
     for operation in ops:
         gate_type = operation[1]
-        qubit = qpu[operation[0]]
+        qubit = qubits[operation[0]]
         gates.append(qubit_op_to_gate(gate_type, qubit))
     moment = [Moment(gates)]
     return Circuit(moment)
 
 
-def qubit_op_to_gate(operation: 'QubitOperator',
-                     qubit) -> 'SingleQubitPauliStringGateOperation':
-    """Convert a qubit operation into a gate operations that can be digested
+def qubit_op_to_gate(
+    operation: "QubitOperator", qubit: Qid
+) -> "SingleQubitPauliStringGateOperation":
+    """Converts a qubit operation into a gate operations that can be digested
     by a Cirq simulator.
 
     Args:
-        operation (QubitOperator)
-        qubit (Qid) - a qubit on which the Pauli matrices will act.
+        operation: Operation represented as a QubitOperator.
+            Either "X", "Y", or "Z".
+        qubit: A qubit on which the Pauli matrices will act.
 
     Returns:
-        (gate) - a gate that can be executed on the qubit passed
+        A Pauli gate operation acting on the qubit.
     """
-    if operation == 'X':
+    if operation == "X":
         return X.on(qubit)
-    if operation == 'Y':
+    if operation == "Y":
         return Y.on(qubit)
-    if operation == 'Z':
+    if operation == "Z":
         return Z.on(qubit)
-    raise ValueError('No gate identified in qubit_op_to_gate')
+    raise ValueError("Invalid operation. Expected 'X', 'Y', or 'Z'.")
 
 
-def qubit_projection(ops: QubitOperator,
-                     qubits: List[LineQubit],
-                     state: numpy.ndarray,
-                     coeff: numpy.ndarray) -> numpy.ndarray:
-    """Find the projection of each set of qubit operators on a
-    wavefunction.
+def qubit_projection(
+    ops: QubitOperator,
+    qubits: List[Qid],
+    state: np.ndarray,
+    coeff: np.ndarray,
+) -> None:
+    """Finds the projection of each set of qubit operators on a
+    wavefunction, modifying the input coeff array.
 
     Args:
-        ops (qubit gates) - A sum of qubit operations which represent the \
-            full ci wavefunction in the qubit basis.
-
-        qubits (Qid) - The qubits of a quantum computer.
-
-        state (numpy.array(dtype=numpy.complex64)) - a cirq wavefunction that \
-            is being projected
-
-        coeff (numpy.array(dtype=numpy.complex64)) - a coeffcient array that \
-            will store the result of the projection.
-
-    Returns:
-        coeff (numpy.array(dtype=numpy.complex64)) - the coefficients modified \
-            in place.
+        ops: Sum of qubit operations which represent the full CI wavefunction
+            in the qubit basis.
+        qubits: Qubits of a quantum computer.
+        state: Cirq wavefunction that is being projected.
+        coeff: Coefficient array that will store the result of the projection.
     """
-    qpu = Simulator(dtype=numpy.complex128)
+    qpu = Simulator(dtype=np.complex128)
     for indx, cluster in enumerate(ops.terms):
         circuit = qubit_ops_to_circuit(cluster, qubits)
         work_state = state.copy()
-        result = qpu.simulate(circuit, qubit_order=qubits,
-                              initial_state=work_state)
+        result = qpu.simulate(
+            circuit, qubit_order=qubits, initial_state=work_state
+        )
         coeff[indx] = result.final_state_vector[0]
 
 
-def qubit_wavefunction_from_vacuum(ops: 'QubitOperator',
-                                   qubits: List['LineQubit']) -> numpy.ndarray:
-    """Generate a cirq wavefunction from the vacuum given qubit operators and a
-    set of qubits that this wavefunction will be represented on
+def qubit_wavefunction_from_vacuum(
+    ops: "QubitOperator", qubits: List[Qid]
+) -> np.ndarray:
+    """Generate a Cirq wavefunction from the vacuum given qubit operators and a
+    set of qubits that this wavefunction will be represented on.
 
     Args:
-        ops (QubitOperators) - a sum of qubit operations with coefficients \
-            scheduling the creation of the qubit wavefunction
-
-        qubits (Qid) - the qubits of the quantum computer
+        ops: Sum of qubit operations with coefficients scheduling the creation
+            of the qubit wavefunction
+        qubits: Qubits of a quantum computer.
 
     Returns:
-        final_state (numpy.array(dtype=numpy.complex64)) - the fully projected \
-            wavefunction in the cirq representation
+        The fully projected wavefunction.
     """
     nqubits = len(qubits)
     vacuum = init_qubit_vacuum(nqubits)
-    final_state = numpy.zeros(2**nqubits, dtype=numpy.complex128)
-    qpu = Simulator(dtype=numpy.complex128)
+    final_state = np.zeros(2 ** nqubits, dtype=np.complex128)
+    qpu = Simulator(dtype=np.complex128)
     for term in ops.terms:
         circuit = qubit_ops_to_circuit(term, qubits)
         state = vacuum.copy()
