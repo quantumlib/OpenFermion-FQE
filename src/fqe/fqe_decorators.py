8077181ba884eceeddb041b90462b44915866399
Toru Shiozaki <shiozaki@qsimulate.com>
2021-05-06 16:29:21 -0400

    Aim to resolve #86, Low filling code requires certain symmetry to be explicitly satisfied by the input tensors. The symmetry was not satisfied by the decorator.

--- src/fqe/fqe_decorators.py
+++ src/fqe/fqe_decorators.py
@@ -19,6 +19,7 @@ intrinsics
 
 from typing import Any, Dict, Tuple, Union, Optional, List
 from functools import wraps
+from itertools import permutations
 
 import copy
 
@@ -261,7 +262,21 @@ def fermionops_tomatrix(ops: 'FermionOperator', norb: int) -> numpy.ndarray:
 
         tensor[tuple(index_mask)] += (-1)**parity * ops.terms[term]
 
-    return tensor
+
+    tensor2 = numpy.zeros_like(tensor)
+    length = 0
+    seed = []
+    for i in range(rank //2):
+        seed.append(i)
+    for i in permutations(seed):
+        iperm = list(i) 
+        jperm = copy.deepcopy(iperm)
+        for j in range(rank // 2):
+            jperm[j] += rank // 2
+        tensor2 += tensor.transpose(iperm + jperm)
+        length += 1
+    tensor2 /= length
+    return tensor2
 
 
 def process_rank2_matrix(mat: numpy.ndarray, norb: int,
